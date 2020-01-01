fc61a98040bd9156a2336ce022fcdb617af38b34
MajesticHero <38969626+Coding-Runner@users.noreply.github.com>
2020-01-01 14:47:17 -0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,22 +3,23 @@ extern crate scraper;
 
 use scraper::{Html, Selector};
 
-fn main() {
-    println!("The Golden State Warriors lost to the Dallas Mavericks: ");
-    scrape_team_data("https://www.scoreboard.com/game/dallas-mavericks-golden-state-warriors-2019-2020/CYWskeOu/#game-summary|game-statistics;0|lineups;1");
+fn main(){
+    println!("The Delhi Capitals are doing great this season as their results lately are the following:");
+    scrape_team_data("https://www.iplt20.com/teams/delhi-capitals/results");
 }
 
-fn scrape_team_data(url: &str) {
+fn scrape_team_data(url:&str){
+
     let mut req = reqwest::get(url).unwrap();
     assert!(req.status().is_success());
     let doc_body = Html::parse_document(&req.text().unwrap());
 
-    let roster = Selector::parse(".scoreboard").unwrap();
+    let team = Selector::parse(".result__outcome").unwrap();
 
-    for roster in doc_body.select(&roster) {
-        let rosters = roster.text().collect::<Vec<_>>();
-        println!("{}",rosters[0]);
+    for team in doc_body.select(&team){
+        let teams = team.text().collect::<Vec<_>>();
+        println!("{}", teams[0]);
     }
-}
 
+}
 
